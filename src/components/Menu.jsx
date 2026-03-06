3b707b21470bd30f925b97e47debb170c2cd9174
Phelipe Almeida da Costa <art0066446@aluno.firjansenaisesi.com.br>
2026-03-06 16:24:04 -0300

    second commit

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,33 +1,16 @@
 import { Link } from 'react-router-dom'
 
 function Menu() {
-  const linkStyle = {
-    marginRight: '16px',
-    textDecoration: 'none',
-    color: '#fff',
-    fontWeight: 'bold',
-  }
-
-  const navStyle = {
-    backgroundColor: '#1e293b',
-    padding: '12px 24px',
-    display: 'flex',
-    alignItems: 'center',
-  }
-
-  const brandStyle = {
-    color: '#38bdf8',
-    fontWeight: 'bold',
-    marginRight: '32px',
-  }
-
   return (
-    <nav style={navStyle}>
-      <span style={brandStyle}>TechSolutions</span>
-      <Link to="/" style={linkStyle}>Home</Link>
-      <Link to="/produtos" style={linkStyle}>Produtos</Link>
-      <Link to="/empresa" style={linkStyle}>Empresa</Link>
-      <Link to="/contato" style={linkStyle}>Contato</Link>
+    <nav>
+      <span className="nav-brand">TechSolutions</span>
+
+      <div className="nav-links">
+        <Link to="/" className="nav-link">Home</Link>
+        <Link to="/produtos" className="nav-link">Produtos</Link>
+        <Link to="/empresa" className="nav-link">Empresa</Link>
+        <Link to="/contato" className="nav-link">Contato</Link>
+      </div>
     </nav>
   )
 }
